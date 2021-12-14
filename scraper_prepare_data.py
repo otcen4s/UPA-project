cccbb3d7d6892ab7700af7ab08dc968eacb3a36a
Jakub Kubik <jakupkubik@gmail.com>
2021-12-14 23:47:54 +0100

    fix saving of dataset  to db

--- scraper_prepare_data.py
+++ scraper_prepare_data.py
@@ -23,13 +23,17 @@ def prepare_data_for_db(measurement, csv_data, unwanted_columnts=[], tags_column
         if tags_columns:
             tags = dict()
             for column in tags_columns:
-                tags.update({column[0]: column[1](record.pop(column[0]))})
+                rec = record.pop(column[0])
+                try:
+                    tags.update({column[0]: column[1](rec)})
+                except ValueError:  # jump over records with empty data
+                    pass
 
         # all items are saved as integer
         for k, v in record.items():
             try:
                 record[k] = int(v)
-            except Exception:
+            except ValueError:
                 record[k] = 0
 
         data_for_db.append({
